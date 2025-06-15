4d4d20ecd36fb23944c93759a68acd172ec6e07b
jofimoljohnson <jofimolj@gmail.com>
2025-06-15 19:57:10 +0530

    fourth commit

--- Server/app.js
+++ Server/app.js
@@ -16,7 +16,7 @@ const app=express()
 
 const PORT=process.env.PORT || 5000
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin:'https://mern-ecommerce-uth7.vercel.app/',
     methods:['GET','POST','PUT','DELETE'],
     allowedHeaders:[
         'Content-Type',
