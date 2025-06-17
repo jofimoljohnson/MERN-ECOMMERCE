6bc37a4b229395eadad14ae2e311158218336788
jofimoljohnson <jofimolj@gmail.com>
2025-06-17 15:37:48 +0530

    seventh commit

--- Server/app.js
+++ Server/app.js
@@ -16,7 +16,8 @@ const app=express()
 
 const PORT=process.env.PORT || 5000
 app.use(cors({
-    origin:'https://mern-ecommerce-chi-rose.vercel.app',
+    origin:['https://mern-ecommerce-chi-rose.vercel.app',
+        "http://localhost:5173"],
     methods:['GET','POST','PUT','DELETE'],
     allowedHeaders:[
         'Content-Type',
@@ -29,6 +30,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use(express.json())
+app.use("/",(req,res)=>res.send("Jofi"))
 
 app.use("/api/auth",authRouter)
 app.use('/api/admin/products',adminProductsRouter)
